57ea570d34768e127e21881487cf0adfe9cde92b
AzatGaniev12 <ganievazat19@gmail.com>
2026-09-20 21:52:39 +0300

    Add files via upload

---
+++ Ball.cpp
@@ -0,0 +1,79 @@
+#include "Ball.hpp"
+#include <cmath>
+
+/**
+ * Задает скорость объекта
+ * @param velocity новое значение скорости
+ */
+void Ball::setVelocity(const Velocity& velocity) {
+    // TODO: место для доработки
+velocity_ = velocity;
+
+}
+
+
+/**
+ * @return скорость объекта
+ */
+Velocity Ball::getVelocity() const {
+    // TODO: место для доработки
+
+    return velocity_;
+}
+
+/**
+ * @brief Выполняет отрисовку объекта
+ * @details объект Ball абстрагирован от конкретного
+ * способа отображения пикселей на экране. Он "знаком"
+ * лишь с интерфейсом, который предоставляет Painter
+ * Рисование выполняется путем вызова painter.draw(...)
+ * @param painter контекст отрисовки
+ */
+void Ball::draw(Painter& painter) const {
+    // TODO: место для доработки
+Color color(1, 1, 1);
+painter.draw(center_ , Radius , color);
+}
+
+/**
+ * Задает координаты центра объекта
+ * @param center новый центр объекта
+ */
+void Ball::setCenter(const Point& center) {
+    // TODO: место для доработки
+center_ = center;
+}
+
+/**
+ * @return центр объекта
+ */
+Point Ball::getCenter() const {
+    // TODO: место для доработки
+
+    return center_;
+}
+
+/**
+ * @brief Возвращает радиус объекта
+ * @details обратите внимание, что метод setRadius()
+ * не требуется
+ */
+double Ball::getRadius() const {
+    // TODO: место для доработки
+double Radius = 4;
+    return {Radius};
+}
+
+/**
+ * @brief Возвращает массу объекта
+ * @details В нашем приложении считаем, что все шары
+ * состоят из одинакового материала с фиксированной
+ * плотностью. В этом случае масса в условных единицах
+ * эквивалентна объему: PI * radius^3 * 4. / 3.
+ */
+double Ball::getMass() const {
+    // TODO: место для доработки
+double Mass = PI * Radius^3 * 4. / 3.;
+
+    return {Mass};
+}
